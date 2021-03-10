6384327f846c79823f75d97c3eb14421896aaa8e
CutlerMcMartin <cutler.mcmartin@gmail.com>
2021-03-09 23:59:17 -0600

    Messing around with props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,22 @@
-const Header = () => {
+import PropTypes from 'prop-types'
+import Button from './Button'
+
+const Header = ({title}) => {
+    
     return (
-        <div>
-            <h1>Task Tracker</h1>            
-        </div>
+            <header className='header'>
+                <h1>{title}</h1> 
+                <Button color='red' text='Add' onClick={onClick}/>
+            </header>
     )
+}
+
+Header.defaultProps = {
+    title: "Task Tracker",
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 }
 
 export default Header
