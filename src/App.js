b658232de52930a0d8b8f254fb0f17928cc68a06
CutlerMcMartin <cutler.mcmartin@gmail.com>
2021-03-17 20:05:44 -0500

    Pushing state from tasks to App Adding FA icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { useState } from 'react'
+
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
 const App = () => {
+
+  const [tasks, setTasks] = useState(
+    [
+      {
+          id: 1,
+          text: "Doctor's Appointment",
+          day: "May 16th"
+      },
+      {
+          id: 2,
+          text: "Doink Appointment",
+          day: "May 15th"
+      }
+    ]
+  );
+
   return (
     <div className="App">
       <Header />
-      <Tasks / >
+      <Tasks tasks={tasks} / >
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,21 +1,12 @@
-import { useState } from 'react'
+import Task from './Task'
 
-const Tasks = () => {
-    const [tasks, setTasks] = useState(
-        [
-            {
-                id: 1,
-                text: "Doctor's Appointment"
-            },
-            {
-                id: 2,
-                text: "Doink Appointment"
-            }
-        ]
-    );
+const Tasks = ({tasks}) => {
     return (
         <>
-          {tasks.map((task) => (<h3 key={task.id}>{task.text}</h3>))}  
+        {tasks.map((task) => (
+            <Task key={task.id} task={task} />
+        ))}
+          
         </>
     )
 }
