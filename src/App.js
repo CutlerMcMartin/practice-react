a29e7a55d1eaa94a7654f8415000b88dc3e50120
CutlerMcMartin <cutler.mcmartin@gmail.com>
2021-03-31 22:58:07 -0500

    Toggle state added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 const App = () => {
-
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState(
     [
       {
@@ -42,7 +42,7 @@ const App = () => {
   return (
     <div className="container">
       <Header />
-      <AddTask onAdd={addTask} />
+      {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? <Tasks tasks={tasks} onDelete = {deleteTask} onToggle={toggleReminder}/> : "No tasks broski"}
     </div>
   );
